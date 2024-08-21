306851fa6da0edd483ad2be8549375b0852a3019
rk <ranjitkale0001@gmail.com>
2024-08-21 18:27:10 +0530

    Create components
    create components
    C:\Users\user\Documents\GitHub\Angular\angular_initial> ng g c MyComponent
    
    call without route
    <app-my-component></app-my-component>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponentComponent } from "./my-component/my-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular_initial';
+  constructor(){
+    // setTimeout(()=>{
+    //      this.title="Project In Angular";
+    // },2000);
+}
 }
